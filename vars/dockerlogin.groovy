6312196360ed3e2741c9b1545496339b1436f2b4
Taye Akinnola <tayeomoakin@gmail.com>
2025-05-13 13:24:28 +0100

    Update dockerlogin.groovy

---
+++ vars/dockerlogin.groovy
@@ -0,0 +1,10 @@
+def call() {
+    echo "Logging into DockerHub..."
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerhub-creds',
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+        sh 'echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin'
+    }
+}
