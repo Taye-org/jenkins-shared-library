4a1a913cb442a8c79ec6b2bf711f74a93d511b26
Taye Akinnola <tayeomoakin@gmail.com>
2025-05-13 13:25:34 +0100

    Update deployVm.groovy

---
+++ vars/deployVm.groovy
@@ -0,0 +1,27 @@
+def call(String branchName, String imageTag, String sshUser, String sshKeyPath, String vmIp, String imageName) {
+    def composeFile = ''
+    if (branchName == 'testing') {
+        composeFile = '/home/tayelolu/pythonapp2/docker-compose.testing.yml'
+    } else if (branchName == 'staging') {
+        composeFile = '/home/tayelolu/pythonapp2/docker-compose.staging.yml'
+    } else if (branchName == 'main') {
+        composeFile = '/home/tayelolu/pythonapp2/docker-compose.yaml'
+    } else {
+        echo "Branch ${branchName} has no deployment config."
+        return
+    }
+
+    sh 'chmod 600 ' + sshKeyPath
+
+    echo "Deploying Docker image to VM..."
+    sh """
+        ssh -o StrictHostKeyChecking=yes -i ${sshKeyPath} ${sshUser}@${vmIp} '
+            cd /home/tayelolu/pythonapp2 &&
+            git fetch origin &&
+            git checkout ${branchName} &&
+            git pull origin ${branchName} &&
+            docker pull ${imageName}:${imageTag} &&
+            docker-compose -f ${composeFile} up -d
+        '
+    """
+}
