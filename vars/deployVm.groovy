292bf05b425c4930413d09774b491eeb605032c2
Taye Akinnola <tayeomoakin@gmail.com>
2025-05-14 09:15:28 +0100

    Update deployVm.groovy

--- vars/deployVm.groovy
+++ vars/deployVm.groovy
@@ -1,35 +1,26 @@
-def call(String branchName, String imageTag, String sshUser, String sshKeyPath, String vmIp, String imageName) {
+def call(String branchName, String imageTag, String SSH_USER, String SSH_KEY_PATH, String VM_IP, String imageName) {
     def composeFile = ''
     if (branchName == 'testing') {
         composeFile = '/home/tayelolu/pythonapp2/docker-compose.testing.yml'
     } else if (branchName == 'staging') {
         composeFile = '/home/tayelolu/pythonapp2/docker-compose.staging.yml'
     } else if (branchName == 'main') {
-        composeFile = '/home/tayelolu/pythonapp2/docker-compose.yaml'
+        composeFile = '/home/tayelolu/pythonapp2/docker-compose.yml'
     } else {
         echo "Branch ${branchName} has no deployment config."
         return
     }
 
-   sh "chmod 600 $SSH_KEY_PATH"
+    sh "chmod 600 ${SSH_KEY_PATH}"
 
-echo "Deploying Docker image to VM..."
+    echo "Deploying Docker image to VM..."
 
-sh """
-    def command = '''
-        ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'bash -s' 
-        cd /home/tayelolu/pythonapp2 &&
-        git fetch origin &&
-        git checkout ${branchName} &&
-        git pull origin ${branchName} &&
-        docker-compose down --volumes --remove-orphans &&
-        docker-compose build --no-cache &&
-        docker pull ${imageName}:${imageTag} &&
-        docker-compose -f ${composeFile} up -d
-        
-    '''
-    sh command
-"""
-
-
+    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'cd /home/tayelolu/pythonapp2'"
+    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'git fetch origin'"
+    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'git checkout ${branchName}'"
+    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'git pull origin ${branchName}'"
+    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'docker-compose down --volumes --remove-orphans'"
+    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'docker-compose build --no-cache'"
+    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'docker pull ${imageName}:${imageTag}'"
+    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'docker-compose -f ${composeFile} up -d'"
 }
