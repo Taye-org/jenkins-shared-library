560d342969e9f217bd6cdd5342b92e72f1e555cd
Taye Akinnola <tayeomoakin@gmail.com>
2025-05-14 12:38:21 +0100

    Update deployVm.groovy

--- vars/deployVm.groovy
+++ vars/deployVm.groovy
@@ -11,17 +11,18 @@ def call(String branchName, String imageTag, String SSH_USER, String SSH_KEY_PAT
         return
     }
 
-   sh "chmod 600 ${SSH_KEY_PATH}"
-
-sh """
-    ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${vmIp} << EOF
-        cd /home/tayelolu/pythonapp2 &&
+   sh """
+    chmod 600 ${SSH_KEY_PATH}
+    echo "Deploying Docker image to VM..."
+    ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} << EOF
+        cd /home/${SSH_USER}/pythonapp2 &&
         git fetch origin &&
         git checkout ${branchName} &&
         git pull origin ${branchName} &&
-        docker pull ${imageName}:${imageTag} &&
+        docker pull ${imageTag} &&
         docker-compose -f ${composeFile} up -d
     EOF
 """
+
 
 }
