5485e0204d090e759e7bb0ea2b43d9496dcf0c61
Taye Akinnola <tayeomoakin@gmail.com>
2025-05-14 13:59:43 +0100

    Update deployVm.groovy

--- vars/deployVm.groovy
+++ vars/deployVm.groovy
@@ -1,31 +1,29 @@
-def call(String branchName, String imageTag, String SSH_USER, String SSH_KEY_PATH, String VM_IP, String imageName) {
+def call(String branchName, String imageTag, String sshUser, String sshKeyPath, String vmIp, String imageName) {
     def composeFile = ''
     if (branchName == 'testing') {
         composeFile = '/home/tayelolu/pythonapp2/docker-compose.testing.yml'
     } else if (branchName == 'staging') {
         composeFile = '/home/tayelolu/pythonapp2/docker-compose.staging.yml'
     } else if (branchName == 'main') {
-        composeFile = '/home/tayelolu/pythonapp2/docker-compose.yml'
+        composeFile = '/home/tayelolu/pythonapp2/docker-compose.yaml'
     } else {
         echo "Branch ${branchName} has no deployment config."
         return
     }
-    
-    echo "DEBUG: sshKeyPath = ${sshKeyPath}"
 
+   sh "chmod 600 $SSH_KEY_PATH"
 
-   sh """
-     sh "chmod 600 ${SSH_KEY_PATH}"
-    echo "Deploying Docker image to VM..."
-    ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} << EOF
-        cd /home/${SSH_USER}/pythonapp2 &&
+echo "Deploying Docker image to VM..."
+
+sh """
+    ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} << 'EOF'
+        cd /home/tayelolu/pythonapp2 &&
         git fetch origin &&
         git checkout ${branchName} &&
         git pull origin ${branchName} &&
-        docker pull ${imageTag} &&
+        docker pull ${imageName}:${imageTag} &&
         docker-compose -f ${composeFile} up -d
     EOF
 """
-
 
 }
