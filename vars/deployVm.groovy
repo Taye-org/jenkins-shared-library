44b4cb78a619aa06872f7d217d4f384cf6558a0c
Taye Akinnola <tayeomoakin@gmail.com>
2025-05-14 12:32:55 +0100

    Update deployVm.groovy

--- vars/deployVm.groovy
+++ vars/deployVm.groovy
@@ -11,16 +11,17 @@ def call(String branchName, String imageTag, String SSH_USER, String SSH_KEY_PAT
         return
     }
 
-    sh "chmod 600 ${SSH_KEY_PATH}"
+   sh "chmod 600 ${SSH_KEY_PATH}"
 
-    echo "Deploying Docker image to VM..."
+sh """
+    ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${vmIp} << EOF
+        cd /home/tayelolu/pythonapp2 &&
+        git fetch origin &&
+        git checkout ${branchName} &&
+        git pull origin ${branchName} &&
+        docker pull ${imageName}:${imageTag} &&
+        docker-compose -f ${composeFile} up -d
+    EOF
+"""
 
-    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'cd /home/tayelolu/pythonapp2'"
-    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'git fetch origin'"
-    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'git checkout ${branchName}'"
-    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'git pull origin ${branchName}'"
-    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'docker-compose down --volumes --remove-orphans'"
-    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'docker-compose build --no-cache'"
-    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'docker pull ${imageName}:${imageTag}'"
-    sh "ssh -o StrictHostKeyChecking=no -i ${SSH_KEY_PATH} ${SSH_USER}@${VM_IP} 'docker-compose -f ${composeFile} up -d'"
 }
